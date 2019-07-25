26412b24a0976905696520ecb4846a362c5d450b
Jon Jagger <jon@jaggersoft.com>
2019-07-25 09:34:37 +0100

    notes update

--- images/avatars/index.sh
+++ images/avatars/index.sh
@@ -5,6 +5,8 @@ set -e
 # index based. Eg alligator.jpg --> 0.jpg
 # The idea is that this decoupling will make it easier
 # to replace the images to switch to a new set of avatars.
+# If you do this, the Avatar.names (in web/app/models)
+# will still hold the animal names though...
 
 AVATAR_NAMES=(
   alligator
