b53537d0a7f994efcd07182846385e025bd3cc33
JonJagger <jon@jaggersoft.com>
2019-08-02 10:55:50 +0100

    [ci skip] refactoring capture duplication in build-image script

--- sh/build_docker_images.sh
+++ sh/build_docker_images.sh
@@ -2,9 +2,22 @@
 set -e
 
 readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
-
 export SHA=$(cd "${ROOT_DIR}" && git rev-parse HEAD)
 
-docker-compose \
-  --file "${ROOT_DIR}/docker-compose.yml" \
-  build
+build_service_image()
+{
+  echo
+  docker-compose \
+    --file "${ROOT_DIR}/docker-compose.yml" \
+      build \
+        "${1}"
+}
+
+build_service_image nginx
+
+# Assuming we do not have any new nginx commits, nginx's latest commit
+# sha will match the image tag inside versioner's .env file.
+# This means we can tag to it and a [cyber-dojo up] call
+# will use the tagged image.
+readonly IMAGE=cyberdojo/nginx
+docker tag ${IMAGE}:latest ${IMAGE}:${SHA:0:7}
