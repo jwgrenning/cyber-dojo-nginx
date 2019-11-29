3e7b6bd0173a4b20010b8037abe74419982b63d9
JonJagger <jon@jaggersoft.com>
2019-11-29 09:08:06 +0000

    Refactor script so image-name is not repeated

--- sh/build_docker_images.sh
+++ sh/build_docker_images.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+export IMAGE_NAME=${1:-cyberdojo/nginx}
+
 readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 export SHA=$(cd "${ROOT_DIR}" && git rev-parse HEAD)
 
@@ -19,6 +21,5 @@ build_service_image nginx
 # sha will match the image tag inside versioner's .env file.
 # This means we can tag to it and a [cyber-dojo up] call
 # will use the tagged image.
-readonly IMAGE=cyberdojo/nginx
-docker tag ${IMAGE}:latest ${IMAGE}:${SHA:0:7}
-docker run --rm ${IMAGE}:latest sh -c 'echo ${SHA}'
+docker tag ${IMAGE_NAME}:latest ${IMAGE_NAME}:${SHA:0:7}
+docker run --rm ${IMAGE_NAME}:latest sh -c 'echo ${SHA}'
