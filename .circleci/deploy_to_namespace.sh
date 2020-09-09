f55decfbb01b630127073e5090e8d041d66da35d
Jon Jagger <jon@jaggersoft.com>
2020-09-09 20:08:45 +0100

    Add env-vars for ports into container

--- .circleci/deploy_to_namespace.sh
+++ .circleci/deploy_to_namespace.sh
@@ -1,19 +1,27 @@
 #!/bin/bash -e
 
+readonly MY_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 readonly NAMESPACE="${1}" # beta|prod
 readonly K8S_URL=https://raw.githubusercontent.com/cyber-dojo/k8s-install/master
 readonly VERSIONER_URL=https://raw.githubusercontent.com/cyber-dojo/versioner/master
 source <(curl "${K8S_URL}/sh/deployment_functions.sh")
 export $(curl "${VERSIONER_URL}/app/.env")
 readonly CYBER_DOJO_NGINX_TAG="${CIRCLE_SHA1:0:7}"
+readonly YAML_VALUES_FILE="${MY_DIR}/k8s-general-values.yml"
 
 gcloud_init
 helm_init
+
+cat "${MY_DIR}/env-var-values.yml" >> "${YAML_VALUES_FILE}"
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+cat "${YAML_VALUES_FILE}"
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+
 helm_upgrade_probe_no_prometheus_no \
    "${NAMESPACE}" \
    "nginx" \
    "${CYBER_DOJO_NGINX_IMAGE}" \
    "${CYBER_DOJO_NGINX_TAG}" \
    "${CYBER_DOJO_NGINX_PORT}" \
-   ".circleci/k8s-general-values.yml" \
-   ".circleci/k8s-${NAMESPACE}-values.yml"
+   "${YAML_VALUES_FILE}"
+   "${MY_DIR}/k8s-${NAMESPACE}-values.yml"
