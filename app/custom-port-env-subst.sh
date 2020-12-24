31a09c61ef03ee57b4152285da635439be96b506
Jon Jagger <jon@jaggersoft.com>
2020-12-24 10:34:48 +0000

    Substitute all cyber-dojo port env-vars.

--- app/custom-port-env-subst.sh
+++ app/custom-port-env-subst.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+# This script is automatically run by the nginx server when it starts.
+# See docker-entrypoint.sh in the root of an nginx container.
+
 readonly template_path=/docker-entrypoint.d/nginx.conf.template
 readonly ports_filename=/docker-entrypoint.d/ports.env
 readonly defined_envs="$(printf '${%s}' $(cat "${ports_filename}" | cut -d= -f1))"
