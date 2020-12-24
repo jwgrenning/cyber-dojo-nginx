e5a9be689c25628586af110920d024fb8c531ffe
Jon Jagger <jon@jaggersoft.com>
2020-12-24 10:00:15 +0000

    Run one hard-wired env-var subst

--- app/custom-port-env-subst.sh
+++ app/custom-port-env-subst.sh
@@ -2,7 +2,12 @@
 
 set -e
 
-# Get empty envsubst in place
+# Run one hard-wired env-var subst
 
 readonly dir=/etc/nginx/conf.d
-envsubst "NOT_YET" < "${dir}/default.conf.template" > "${dir}/default.conf"
+
+export CYBER_DOJO_SHAS_PORT=4522
+
+readonly defined_envs="$(printf '${%s} ' $(env | cut -d= -f1))"
+
+envsubst "${defined_envs}" < "${dir}/default.conf.template" > "${dir}/default.conf"
