7130904287c9bffd77b92e79a1cdb6ee356d4909
Jon Jagger <jon@jaggersoft.com>
2020-12-24 10:07:36 +0000

    Copy template file into /docker-entrypoint.d dir.

--- app/custom-port-env-subst.sh
+++ app/custom-port-env-subst.sh
@@ -4,10 +4,10 @@ set -e
 
 # Run one hard-wired env-var subst
 
-readonly dir=/etc/nginx/conf.d
-
 export CYBER_DOJO_SHAS_PORT=4522
 
 readonly defined_envs="$(printf '${%s} ' $(env | cut -d= -f1))"
 
-envsubst "${defined_envs}" < "${dir}/default.conf.template" > "${dir}/default.conf"
+readonly template_path=/docker-entrypoint.d/nginx.conf.template
+
+envsubst "${defined_envs}" < "${template_path}" > "/etc/nginx/conf.d/default.conf"
