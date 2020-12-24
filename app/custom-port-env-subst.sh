ba30586063fe65be8cb0e334e1abace045b66f82
Jon Jagger <jon@jaggersoft.com>
2020-12-24 10:20:56 +0000

    Get port env-var names and values from file.

--- app/custom-port-env-subst.sh
+++ app/custom-port-env-subst.sh
@@ -1,18 +1,8 @@
 #!/bin/sh
-
 set -e
 
-# Run one hard-wired env-var subst
-
-echo_cyber_dojo_ports()
-{
-  echo CYBER_DOJO_SHAS_PORT=4522
-}
-
-export $(echo_cyber_dojo_ports)
-
-readonly defined_envs="$(printf '${%s} ' $(env | cut -d= -f1))"
-
 readonly template_path=/docker-entrypoint.d/nginx.conf.template
-
+readonly ports_filename=/docker-entrypoint.d/ports.env
+readonly defined_envs="$(printf '${%s}' $(cat "${ports_filename}" | cut -d= -f1))"
+export $(cat "${ports_filename}")
 envsubst "${defined_envs}" < "${template_path}" > "/etc/nginx/conf.d/default.conf"
